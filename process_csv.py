6eced5d1c74004745613a10c8e9543826f67bb59
ARSL <matthijs.schrage@yahoo.com>
2021-11-12 12:19:29 +0100

    More features

--- process_csv.py
+++ process_csv.py
@@ -5,8 +5,11 @@ from ast import literal_eval as make_tuple
 
 # METADATA = "C:\\Users\\Matthijs\\PycharmProjects\\AI_Actuated_Microswarm_4\\Include\\AI_Actuated_Micrswarm_4\\develop_model\\Metadata.csv"
 METADATA = "metadata_for_new_model4_tracked2.csv"
-PROCESSED_CSV = "training_data\\metadata_for_new_model4_tracked2_processed"
+PROCESSED_CSV = "training_data_with_forces\\metadata_for_new_model4_tracked2_processed_forces"
 NUM_CLUSTER = 50
+
+def calc_force(pos1, pos2, size1, size2):
+    return  ((size1 * size2) ** 1.5) / (np.linalg.norm(pos1 - pos2) ** 2)
 
 if __name__ == "__main__":
 
@@ -73,6 +76,19 @@ if __name__ == "__main__":
 
         datapoint1 = metadata.iloc[n+closest]
 
+        stuff = np.array([make_tuple(datapoint[f'Cluster{n}']) for n in range(50)])
+        # print(stuff)
+        forces = np.zeros((50, 50))
+
+        for i, k in enumerate(stuff):
+            pos1, size1 = k
+            for j, l in enumerate(stuff):
+                pos2, size2 = l
+                force = calc_force(np.array(pos1), np.array(pos2), size1, size2)
+                if force == np.inf:
+                    force = 0
+                forces[i, j] = force
+
         for i in range(NUM_CLUSTER):
 
             data = {"Time": time,
@@ -81,6 +97,8 @@ if __name__ == "__main__":
                     "Action": new_action}
 
             pos, size = make_tuple(datapoint[f"Cluster{i}"])
+            # print('__')
+            # print(pos, size)
             pos1, _ = make_tuple(datapoint1[f"Cluster{i}"])
 
             data.__setitem__("Cluster", i)
@@ -89,6 +107,10 @@ if __name__ == "__main__":
             data.__setitem__("Y0", pos[1])
             data.__setitem__("X1", pos1[0])
             data.__setitem__("Y1", pos1[1])
+
+            for j, force in enumerate(range(50)):
+                # print()
+                data.__setitem__(f'Force_{j}', forces[i, j])
 
             if None in data.values():
                 continue
